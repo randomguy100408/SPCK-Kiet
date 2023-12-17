af055832cdc9cad05292a7c37c14428cb7b1e4e0
randomguy100408 <147293466+randomguy100408@users.noreply.github.com>
2023-12-17 11:57:21 +0700

    ea
    sport

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,31 @@
 const strHeight = document.getElementById("height")
-const positionPlayer = document.getElementById("strPosition")
-const nationPlayer = document.getElementById("strNationality")
-const teamplayer = document.getElementById("strTeam")
-const namePlayer = document.getElementById("name")
+const strPosition = document.getElementById("position")
+const strNationality = document.getElementById("nation")
+const strTeam = document.getElementById("club")
+const strPlayer = document.getElementById("name")
+const strWeight = document.getElementById("weight")
+const strNumber = document.getElementById("number")
+const strGender = document.getElementById("gender")
+const strSide = document.getElementById("side")
+const dateBorn = document.getElementById("born")
+const idPlayer = document.getElementById("playerPic")
+const strKit = document.getElementById("kit")
 searchPlayer.addEventListener("change", function (e) {
     fetch(`https://thesportsdb.com/api/v1/json/3/searchplayers.php?p=${e.target.value}`)
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            strHeight.innerHTML = data.height 
+            strHeight.innerHTML = data.player[0].strHeight.substring(0, 6) || '--'
+            strPosition.innerHTML = data.player[0].strPosition || '--'
+            strPlayer.innerHTML = data.player[0].strPlayer || '--'
+            strWeight.innerHTML = data.player[0].strWeight.substring(0,5) || '--'
+            strNumber.innerHTML = data.player[0].strNumber || '--'
+            strGender.innerHTML = data.player[0].strGender || '--'
+            strSide.innerHTML = data.player[0].strSide || '--'
+            dateBorn.innerHTML = data.player[0].dateBorn || '--'
+            strKit.innerHTML = data.player[0].strKit.substring(0, 4) || '--'
+            idPlayer.src = data.player[0].strCutout || "https://s1.fifaaddict.com/fo4/players/ypypbgak.png?20230921"
+            strNationality.innerHTML = data.player[0].strNationality || '--'
+            strTeam.innerHTML = data.player[0].strTeam || '--'
         })
 })
