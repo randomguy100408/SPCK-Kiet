d68fa1f2289289a868e29673ea695133e3a39092
randomguy100408 <147293466+randomguy100408@users.noreply.github.com>
2023-12-17 09:38:34 +0700

    e
    ea

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-const number = document.getElementById("strNumber")
-const position = document.getElementById("strPosition")
-const nation = document.getElementById("strNationality")
-const team = document.getElementById("strTeam")
-const name = document.getElementById("strPlayer")
+const strHeight = document.getElementById("height")
+const positionPlayer = document.getElementById("strPosition")
+const nationPlayer = document.getElementById("strNationality")
+const teamplayer = document.getElementById("strTeam")
+const namePlayer = document.getElementById("name")
 searchPlayer.addEventListener("change", function (e) {
     fetch(`https://thesportsdb.com/api/v1/json/3/searchplayers.php?p=${e.target.value}`)
-        .then((res) => res.json())
-        .then((data) => {
-
+        .then(res => res.json())
+        .then(data => {
             console.log(data)
+            strHeight.innerHTML = data.height 
         })
 })
